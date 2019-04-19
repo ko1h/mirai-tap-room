425ae84ebe707082b1e4628009d94a4cdde7fd9e
kohichi <kohichi.horii@gmail.com>
2019-04-19 13:42:53 -0700

    add more input so user can add email also add an submit feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,16 @@ function Contact() {
   return(
     <div>
       <h1>contact take us</h1>
-      <input type="text" name="name"></input>
+      <label for="firstName">first name:</label>
+      <input type="text" name="firstName"></input>
+
+      <label for="lastName">last name:</label>
+      <input type="text" name="lastName"></input>
+
+      <label for="email">email:</label>
+      <input type="email" name="email"></input>
+
+      <button type="submit">submit</button>
     </div>
   );
 }
