fef8af66da1ca7e95eaf7333656a7d332f7bd258
kohichi <kohichi.horii@gmail.com>
2019-04-28 21:22:59 -0700

    Change link img and working with styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import fan from '../assets/images/fan.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import facebook from '../assets/images/facebook.png'
+import instagram from '../assets/images/instagram.png'
+import twitter from '../assets/images/twitter.png'
 
 function Header(){
   const linkStyle ={
     color: '#F2EFE9',
     textDecoration: 'none',
-    margin: '10px'
+    hover: 'red',
+    fontSize: '1.5em'
+  }
+  const anchorStyle = {
+    display: 'flex',
+    justifyContent: 'center'
   }
   return (
     <div>
       <style jsx>{`
             div {
               height: 10vh;
+              width: 100%;
               background-color: #68828C;
               display: flex;
               justify-content: space-around;
               align-items: center;
-            }
+            },
             img {
-              height: 50px;
+              height: 40px;
               align-items: center;
             }
               `}</style>
-      <img src={fan}/>
       <div>
-        <Link to="/" style={linkStyle}>Home</Link>
-        <Link to="/sakeList" style={linkStyle}>Sake List</Link>
+        <img src={fan}/>
       </div>
       <div>
-        <FontAwesomeIcon icon="beer" />
+        <Link to="/" style={linkStyle}>HOME</Link> |
+        <Link to="/sakeList" style={linkStyle}>SAKE LIST</Link>
+      </div>
+      <div style={anchorStyle}>
+        <a href="#"><img src={facebook}/></a>
+        <a href="#"><img src={instagram}/></a>
+        <a href="#"><img src={twitter}/></a>
       </div>
     </div>
   )
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import fan from '../assets/images/fan.png'
+import fan from '../assets/images/ceramics.jpeg'
 
 function Welcome() {
   return(
@@ -11,14 +11,16 @@ function Welcome() {
           height: 80vh;
         },
         h1 {
-          
           color: #C1D4D9;
           margin: 0;
           text-align: center;
+        },
+        img {
+          height: 80px;
         }
       `}</style>
-      <img src={fan}/>
       <h1>MIRAI</h1>
+      <img src={fan}/>
     </div>
   )
 }
